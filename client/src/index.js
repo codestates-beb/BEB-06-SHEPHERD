7a4b318366aeb5042c43e5f06bf2de3b21e4898f
choijinyoung <chio0674@naver.com>
2022-11-21 10:07:01 +0900

    feat: Header 라우팅 기능 추가

--- client/src/index.js
+++ client/src/index.js
@@ -24,7 +24,12 @@ import Dashboard from 'pages/Dashboard';
 import Error from 'pages/Error';
 import Main from 'pages/Main';
 import Transaction from 'pages/Transaction';
-import App from './App';
+import App from 'App'; //상대 경로로 되어있어서 절대경로로 변경
+
+// components
+import About from 'components/About';
+import TransactionDetail from 'components/TransactionDetail';
+import MakeTransfer from 'components/MakeTransfer';
 
 // Router 라우터
 const router = createBrowserRouter([
@@ -34,7 +39,7 @@ const router = createBrowserRouter([
     errorElement: <Error />,
     children: [
       {
-        path: 'contact/',
+        path: 'contact/', //==
         element: <Contact />
       },
       {
@@ -48,6 +53,18 @@ const router = createBrowserRouter([
       {
         path: '/',
         element: <Main />
+      },
+      {
+        path: 'about/',
+        element: <About />
+      },
+      {
+        path: 'transactiondetail/',
+        element: <TransactionDetail />
+      },
+      {
+        path: 'maketransfer/',
+        element: <MakeTransfer />
       }
     ]
   }
