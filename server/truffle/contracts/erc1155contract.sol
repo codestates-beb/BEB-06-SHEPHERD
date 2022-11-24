bd41b10fc7705915291cb84eb24560071590260f
yeonvv <yeonvv8@gmail.com>
2022-11-25 00:25:53 +0900

    build: flowchart변경에 따른 build 수정

---
+++ server/truffle/contracts/erc1155contract.sol
@@ -0,0 +1,22 @@
+// contracts/GameItems.sol
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.17;
+
+import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
+import "@openzeppelin/contracts/GSN/GSNRecipient.sol";
+
+contract SteelTracker is ERC1155, GSNRecipient {
+    uint256 public constant GOLD = 0;
+    uint256 public constant SILVER = 1;
+    uint256 public constant THORS_HAMMER = 2;
+    uint256 public constant SWORD = 3;
+    uint256 public constant SHIELD = 4;
+
+    constructor() public ERC1155() {
+        _mint(msg.sender, GOLD, 10**18, "");
+        _mint(msg.sender, SILVER, 10**27, "");
+        _mint(msg.sender, THORS_HAMMER, 1, "");
+        _mint(msg.sender, SWORD, 10**9, "");
+        _mint(msg.sender, SHIELD, 10**9, "");
+    }
+};
