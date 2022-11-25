c80626747babbf25c77920b7042b1a7f014570dd
yeonvv <yeonvv8@gmail.com>
2022-11-25 17:29:14 +0900

    feat: login

--- server/models/user.models.js
+++ server/models/user.models.js
@@ -1,16 +1,20 @@
 const mongoose = require("mongoose");
+const uniqueValidator = require("mongoose-unique-validator");
 
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
   name: { type: String, required: true },
   email: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
+  password: { type: String, required: true, minlength: 6 },
   account: { type: String, required: true },
   address: { type: String, required: true },
   sendOrder: { type: String, required: true },
   takeOrder: { type: String, required: true },
 });
+
+// 중복 유효성 검사
+userSchema.plugin(uniqueValidator);
 
 // 첫번째 인자 : 모델 이름 (대문자로 시작, 단수형으로 선언)
 // 두번째 인자 : 모델에 참조할 스키마
