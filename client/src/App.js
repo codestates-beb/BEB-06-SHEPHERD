4d63d3411b6aa162a98c6de1259bd85bd4f37459
Heein-Park <gmldls94@gmail.com>
2022-11-29 14:55:25 +0900

    feat: login context

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,13 @@
 // Components
 import BaseLayout from 'components/base/BaseLayout';
-import PrivateRoute from 'components/PrivateRoute';
+import { CurrentUserContext } from 'Contexts';
 
 // Modules
 import { Outlet } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+
+// Custom Modules
+import * as schema from 'features/schema';
 
 /*
   Outlet 컴포넌트는 index.js에서 지정한 children 페이지를 불러와
@@ -11,10 +15,26 @@ import { Outlet } from 'react-router-dom';
 */
 
 function App () {
+  const [currentUser, setCurrentUser] = useState({
+    id: '',
+    password: ''
+  });
+
+  useEffect(() => {
+    schema.user.validate(currentUser);
+    console.log(currentUser);
+  }, [currentUser]);
+
   return (
-    <BaseLayout>
-      <Outlet />
-    </BaseLayout>
+    <CurrentUserContext.Provider value={{
+      currentUser,
+      setCurrentUser
+    }}
+    >
+      <BaseLayout>
+        <Outlet />
+      </BaseLayout>
+    </CurrentUserContext.Provider>
   );
 }
 
