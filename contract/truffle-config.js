db38b20bd375d9229fae6970028ac9d19e200a79
Heein-Park <gmldls94@gmail.com>
2022-11-30 18:46:30 +0900

    style: semiStandard

--- contract/truffle-config.js
+++ contract/truffle-config.js
@@ -65,12 +65,12 @@ module.exports = {
     // options below to some value.
     //
     development: {
-     host: "127.0.0.1",     // Localhost (default: none)
-     port: 7545,            // Standard Ethereum port (default: none)
-     network_id: "5777",
-     //gas: "0x1ffffffffffffe",
-     //gasPrice: 0,      // Any network (default: none)
-    },
+      host: '127.0.0.1', // Localhost (default: none)
+      port: 7545, // Standard Ethereum port (default: none)
+      network_id: '5777'
+      // gas: "0x1ffffffffffffe",
+      // gasPrice: 0,      // Any network (default: none)
+    }
     //
     // An additional network, but with some advanced options…
     // advanced: {
@@ -108,7 +108,7 @@ module.exports = {
   // Configure your compilers
   compilers: {
     solc: {
-      version: "0.8.17" // Fetch exact version from solc-bin (default: truffle's version)
+      version: '0.8.17' // Fetch exact version from solc-bin (default: truffle's version)
       // docker: true,        // Use "0.5.1" you've installed locally with docker (default: false)
       // settings: {          // See the solidity docs for advice about optimization and evmVersion
       //  optimizer: {
