09af558e4de57d1fbbafed9c833f8e54b9a3109e
Heein-Park <gmldls94@gmail.com>
2022-12-01 03:09:44 +0900

    style: semiStandard & remove the unnecessary

--- server/routes/main.route.js
+++ server/routes/main.route.js
@@ -1,14 +1,14 @@
-"use strict";
+'use strict';
 
-const mainControllers = require("../controllers/main.controllers");
-const checkAuth = require("../middleware/check-auth");
+const mainControllers = require('../controllers/main.controllers');
+const checkAuth = require('../middleware/check-auth');
 
-const router = require("express").Router();
+const router = require('express').Router();
 
-router.get("/", mainControllers.main);
+router.get('/', mainControllers.main);
 
 router.use(checkAuth);
 
-router.get("/faucet/:account", mainControllers.faucet);
+router.get('/faucet/:account', mainControllers.faucet);
 
 module.exports = router;
--- server/routes/user.route.js
+++ server/routes/user.route.js
@@ -1,15 +1,12 @@
 'use strict';
 
 const userController = require('../controllers/user.controllers');
-// const checkAuth = require("../middleware/check-auth");
 
 const router = require('express').Router();
 
 router.post('/join', userController.join);
 router.post('/login', userController.login);
 router.get('/:uid', userController.userInfo);
-
-// router.use(checkAuth);
 
 router.patch('/:uid', userController.addAccount);
 
