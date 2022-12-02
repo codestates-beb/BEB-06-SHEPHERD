ed3ff37db3d83d848a14093eddb7da0c378c68cd
Heein-Park <gmldls94@gmail.com>
2022-12-02 11:51:07 +0900

    refactor: replace dummy

--- client/src/components/TransactionList.js
+++ client/src/components/TransactionList.js
@@ -3,6 +3,7 @@ Trasaction List마다 useState를 두고 컴포넌트 렌더링 타이밍에 데
 */
 
 // Modules
+import Axios from 'axios';
 import parseObject from 'features/parseObject';
 import * as schema from 'features/schema';
 import { useEffect, useState } from 'react';
@@ -94,48 +95,42 @@ function TransactionItem ({ data }) {
   );
 }
 
-function TransactionList () {
+function TransactionList ({ user }) {
   const [transactions, setTransactions] = useState([]);
 
   const loadList = () => {
-    setTransactions([
-      {
-        id: 1,
-        arrival: {
-          date: new Date(2022, 11, 22),
-          location: '대구'
-        },
-        currentLocation: '포항',
-        departure: {
-          date: new Date(2022, 11, 21),
-          location: '포항'
-        },
-        orderDate: new Date(2022, 11, 18),
-        orderer: '건양엔지니어링',
-        status: 'Ongoing',
-        amount: 100
-      },
-      {
-        id: 2,
-        arrival: {
-          date: new Date(2022, 11, 22),
-          location: '대구'
-        },
-        currentLocation: '포항',
-        departure: {
-          date: new Date(2022, 11, 21),
-          location: '포항'
-        },
-        orderDate: new Date(2022, 11, 18),
-        orderer: '건양엔지니어링',
-        status: 'Ongoing',
-        amount: 100
-      }
-    ]);
+    Axios.get(`${process.env.REACT_APP_API_URL}/tx/getTxInfo`)
+      .then(response => {
+        const { data } = response;
+        const { queryTxInfo } = data;
+
+        const transactions = queryTxInfo.map(() => {
+          return {
+            id: 1,
+            arrival: {
+              date: new Date(2022, 11, 22),
+              location: '대구'
+            },
+            currentLocation: '포항',
+            departure: {
+              date: new Date(2022, 11, 21),
+              location: '포항'
+            },
+            orderDate: new Date(2022, 11, 18),
+            orderer: '건양엔지니어링',
+            status: 'Ongoing',
+            amount: 100
+          };
+        });
+        setTransactions(transactions);
+      }).catch(error => {
+        console.error(error);
+        setTransactions([]);
+      });
   };
 
   // Transaction 목록을 불러올 때마다 적용함
-  useEffect(loadList, []);
+  useEffect(loadList, [user]);
 
   const validationTest = (
     Array.isArray(transactions) &&
