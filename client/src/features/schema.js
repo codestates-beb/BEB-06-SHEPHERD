14266062cc35d8ad94a6cb424867f03f6b7ddcdd
Heein-Park <gmldls94@gmail.com>
2022-12-03 03:32:07 +0900

    feat: transaction list

--- client/src/features/schema.js
+++ client/src/features/schema.js
@@ -2,49 +2,44 @@ import Schema from 'validate';
 
 export const transaction = new Schema(
   {
-    id: {
+    blockNumber: {
       type: Number,
       required: true
     },
-    amount: {
+    blockHash: {
+      type: String,
+      required: true
+    },
+    transactionHash: {
+      type: String,
+      required: true
+    },
+    value: {
       type: Number,
       required: true
     },
     arrival: {
-      date: {
-        type: Date,
-        required: true
-      },
       location: {
+        type: String,
+        required: false
+      },
+      accountAddress: {
         type: String,
         required: true
       }
-    },
-    currentLocation: {
-      type: String,
-      required: true
     },
     departure: {
-      date: {
-        type: Date,
-        required: true
-      },
       location: {
+        type: String,
+        required: false
+      },
+      accountAddress: {
         type: String,
         required: true
       }
-    },
-    orderDate: {
-      type: Date,
-      required: true
     },
     orderer: {
       type: String,
-      required: true
-    },
-    status: {
-      type: String,
-      enum: ['Ongoing', 'Ready', 'Complete'],
       required: true
     }
   }
