bb3998209b4a9a2a96e2126184fafcd5b1bff0bb
CDDWNE <103885896+CDDWNE@users.noreply.github.com>
2022-12-03 19:38:25 +0900

    feat: main page 내용 추가

--- client/src/pages/Main.js
+++ client/src/pages/Main.js
@@ -1,11 +1,59 @@
-import Box from '@mui/material/Box';
-import MapChart from 'components/MapChart';
+import CssBaseline from '@mui/material/CssBaseline';
+import Grid from '@mui/material/Grid';
+import Container from '@mui/material/Container';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import MainFeaturedPost from 'components/MainFeaturedPost';
+import FeaturedPost from 'components/FeaturedPost';
 
-function Main () {
+//blog
+
+const mainFeaturedPost = {
+  title: 'Title of a longer featured blog post',
+  description:
+    "Multiple lines of text that form the lede, informing new readers quickly and efficiently about what's most interesting in this post's contents.",
+  image: 'https://source.unsplash.com/random',
+  imageText: 'main image description',
+  linkText: 'Continue reading…',
+};
+
+const featuredPosts = [
+  {
+    title: 'Featured post',
+    date: 'Nov 12',
+    description:
+      'This is a wider card with supporting text below as a natural lead-in to additional content.',
+    image: 'https://source.unsplash.com/random',
+    imageLabel: 'Image Text',
+  },
+  {
+    title: 'Post title',
+    date: 'Nov 11',
+    description:
+      'This is a wider card with supporting text below as a natural lead-in to additional content.',
+    image: 'https://source.unsplash.com/random',
+    imageLabel: 'Image Text',
+  },
+];
+
+const theme = createTheme();
+
+function Main() {
   return (
-    <Box>
-      <MapChart />
-    </Box>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Container maxWidth="lg">
+        <main>
+          <MainFeaturedPost post={mainFeaturedPost} />
+          <Grid Container spacing={4}>
+            {featuredPosts.map((post) => (
+              <FeaturedPost key={post.title} post={post} />
+            ))}
+          </Grid>
+          <Grid container spacing={5} sx={{ mt: 3 }}>
+          </Grid>
+        </main>
+      </Container>
+    </ThemeProvider>
   );
 }
 
